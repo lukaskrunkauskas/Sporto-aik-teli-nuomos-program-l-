a43b5582da027af8e1103e6d02e649755d18f58a
Domantas <domantas.aukstuolis@stud.vgtu.lt>
2022-04-02 18:45:38 +0300

    Prideti papildomi mygtukai(Sistemos administravimas, profilio redagavimas, atsijungimo mygtukas)

--- CourseSys/src/main/java/com/example/sportoAiksteliuRezervacija/fxControllers/MainWindow.java
+++ CourseSys/src/main/java/com/example/sportoAiksteliuRezervacija/fxControllers/MainWindow.java
@@ -1,21 +1,30 @@
 package com.example.sportoAiksteliuRezervacija.fxControllers;
 
+import com.example.sportoAiksteliuRezervacija.StartGui;
 import com.example.sportoAiksteliuRezervacija.ds.Court;
+import com.example.sportoAiksteliuRezervacija.ds.User;
 import com.example.sportoAiksteliuRezervacija.ds.enums.CityType;
 import com.example.sportoAiksteliuRezervacija.ds.enums.CourtType;
+import com.example.sportoAiksteliuRezervacija.ds.enums.UserType;
 import com.example.sportoAiksteliuRezervacija.hibernateControllers.CourtHibControl;
+import com.example.sportoAiksteliuRezervacija.hibernateControllers.UserHibControl;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
 
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import java.io.IOException;
 import java.net.URL;
 import java.sql.SQLException;
 import java.util.ArrayList;
@@ -39,9 +48,12 @@ public class MainWindow implements Initializable {
     public ComboBox cityComboBox;
     @FXML
     public ComboBox typeComboBox;
+    @FXML
+    public Button systemAdministrationButton;
 
     EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("CourseSystemMng");
     CourtHibControl courtHibControl = new CourtHibControl(entityManagerFactory);
+    UserHibControl userHibControl = new UserHibControl(entityManagerFactory);
     ObservableList<Court> observableList = FXCollections.observableArrayList();
     ObservableList<String> observableListComboCity = FXCollections.observableArrayList();
     ObservableList<String> observableListComboType = FXCollections.observableArrayList();
@@ -82,6 +94,10 @@ public class MainWindow implements Initializable {
             observableListComboType.add("MANIEZAS");
             observableListComboType.add("VISI");
             typeComboBox.setItems(observableListComboType);
+            // TODO is logino lango gauti userid pakeisti eilute
+            User user = userHibControl.getUserById(1);
+            if(user.getUserType().equals(UserType.USER)) systemAdministrationButton.setVisible(false);
+            else systemAdministrationButton.setVisible(true);
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -109,5 +125,32 @@ public class MainWindow implements Initializable {
             }
         }
         courtTable.setItems(observableList);
+    }
+    //TODO paduoti userio id
+    public void moveToSystemAdministration(ActionEvent actionEvent) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(StartGui.class.getResource("administration-window.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        Stage stage = (Stage) systemAdministrationButton.getScene().getWindow();
+        stage.setTitle("Admin");
+        stage.setScene(scene);
+        stage.show();
+    }
+    //TODO pakeisti log-in fxml
+    public void logOut(ActionEvent actionEvent) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(StartGui.class.getResource("log-in.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        Stage stage = (Stage) systemAdministrationButton.getScene().getWindow();
+        stage.setTitle("Log in");
+        stage.setScene(scene);
+        stage.show();
+    }
+    //TODO pakeisti profile edi fxml
+    public void moveToProfileEdit(ActionEvent actionEvent) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(StartGui.class.getResource("profile-edit.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        Stage stage = (Stage) systemAdministrationButton.getScene().getWindow();
+        stage.setTitle("Profile edit");
+        stage.setScene(scene);
+        stage.show();
     }
 }
