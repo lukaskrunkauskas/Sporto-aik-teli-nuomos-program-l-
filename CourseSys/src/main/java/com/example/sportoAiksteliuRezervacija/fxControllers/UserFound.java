92c8687479bc08daa587b0bc4661b1ecc6e596bf
ExLean <paulius0macys@gmail.com>
2022-11-27 19:27:22 +0200

    * Prideti userView lange nuotrauka

--- CourseSys/src/main/java/com/example/sportoAiksteliuRezervacija/fxControllers/UserFound.java
+++ CourseSys/src/main/java/com/example/sportoAiksteliuRezervacija/fxControllers/UserFound.java
@@ -9,6 +9,7 @@ import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 
@@ -26,7 +27,7 @@ public class UserFound implements Initializable {
     @FXML
     public TextField clientBio;
     @FXML
-    //public ImageView clientPicture;
+    public ImageView clientPicture;
 
     private int userId;
 
@@ -38,7 +39,8 @@ public class UserFound implements Initializable {
         this.clientUsername.setText(userHibControl.getUserById(pickedUserId).getUsername());
         this.clientEmail.setText(userHibControl.getUserById(pickedUserId).getEmail());
         this.clientBio.setText(userHibControl.getUserById(pickedUserId).getInformation());
-//        this.clientPicture = clientPicture;
+        Image image = new Image(userHibControl.getUserById(pickedUserId).getPictureUrl());
+        this.clientPicture.setImage(image);
     }
 
     public void goBack(ActionEvent actionEvent) throws IOException {
